8a1ee069aee9960e1b84bf72e335e032e8e5882f
pauytrh2 <pauytrh@gmail.com>
2025-06-12 13:24:32 +0300

    Improve text/mod.rs

--- src/text/mod.rs
+++ src/text/mod.rs
@@ -1,9 +1,13 @@
 use macroquad::{color::BLACK, text::draw_text};
 
-pub fn draw_all_text(player_x: f32, player_y: f32, player_can_dash: bool) {
-    let position_text = format!("X: {}, Y: {}", player_x, player_y);
-    let dash_text = format!("Can Dash: {}", player_can_dash);
-
-    draw_text(&position_text, 10.0, 20.0, 20.0, BLACK);
-    draw_text(&dash_text, 10.0, 40.0, 20.0, BLACK);
+pub fn draw_all_text(x: f32, y: f32, can_dash: bool) {
+    draw_text(&format!("Player X: {:.1}", x), 10.0, 20.0, 20.0, BLACK);
+    draw_text(&format!("Player Y: {:.1}", y), 10.0, 40.0, 20.0, BLACK);
+    draw_text(
+        &format!("Dash Ready: {}", can_dash),
+        10.0,
+        60.0,
+        20.0,
+        BLACK,
+    );
 }
