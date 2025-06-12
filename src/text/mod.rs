d9c573433305f0e8a2415fef4556db7e10d33ee8
pauytrh2 <pauytrh@gmail.com>
2025-06-12 12:59:47 +0300

    Run "cargo fmt"

--- src/text/mod.rs
+++ src/text/mod.rs
@@ -5,5 +5,5 @@ pub fn draw_all_text(player_x: f32, player_y: f32, player_can_dash: bool) {
     let dash_text = format!("Can Dash: {}", player_can_dash);
 
     draw_text(&position_text, 10.0, 20.0, 20.0, BLACK);
-    draw_text(&dash_text, 10.0,40.0, 20.0, BLACK);
+    draw_text(&dash_text, 10.0, 40.0, 20.0, BLACK);
 }
