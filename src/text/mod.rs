0347123518bf0c7482d98e2664e6d068a358c64e
pauytrh2 <pauytrh@gmail.com>
2025-06-12 14:09:41 +0300

    Modify text

--- src/text/mod.rs
+++ src/text/mod.rs
@@ -3,11 +3,5 @@ use macroquad::{color::BLACK, text::draw_text};
 pub fn draw_all_text(x: f32, y: f32, can_dash: bool) {
     draw_text(&format!("Player X: {:.1}", x), 10.0, 20.0, 20.0, BLACK);
     draw_text(&format!("Player Y: {:.1}", y), 10.0, 40.0, 20.0, BLACK);
-    draw_text(
-        &format!("Dash Ready: {}", can_dash),
-        10.0,
-        60.0,
-        20.0,
-        BLACK,
-    );
+    draw_text(&format!("Can Dash: {}", can_dash), 10.0, 60.0, 20.0, BLACK);
 }
