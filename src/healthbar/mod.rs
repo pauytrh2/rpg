72a97d5456a84d21ea6af28d9ea624a22f89ca7c
pauytrh2 <pauytrh@gmail.com>
2025-07-05 18:09:19 +0300

    Fix healthbar positioning when in fullscreen

--- src/healthbar/mod.rs
+++ src/healthbar/mod.rs
@@ -1,15 +1,15 @@
 use macroquad::{
     color::{Color, RED},
     shapes::draw_rectangle,
+    window::{screen_height, screen_width},
 };
 
 use crate::player::Player;
 
-const X: f32 = 690.0;
-const Y: f32 = 580.0;
 const WIDTH: f32 = 100.0;
 const HEIGHT: f32 = 8.0;
 const COLOR: Color = RED;
+const PADDING: f32 = 10.0;
 
 pub struct Healthbar {
     health: f32,
@@ -21,10 +21,12 @@ impl Healthbar {
     }
 
     pub fn update(&mut self, player: &Player) {
-        self.health = player.health
+        self.health = player.health;
     }
 
     pub fn draw(&self) {
-        draw_rectangle(X, Y, WIDTH, HEIGHT, COLOR);
+        let x = screen_width() - WIDTH - PADDING;
+        let y = screen_height() - HEIGHT - PADDING;
+        draw_rectangle(x, y, WIDTH, HEIGHT, COLOR);
     }
 }
